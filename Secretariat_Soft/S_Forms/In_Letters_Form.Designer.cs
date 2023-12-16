fb69d744e81853a1e292993297ae9322f381d066
Iggy988 <igor.rodic1988@gmail.com>
2023-12-16 22:19:14 +0100

    Customizing DataGridView

--- Secretariat_Soft/S_Forms/In_Letters_Form.Designer.cs
+++ Secretariat_Soft/S_Forms/In_Letters_Form.Designer.cs
@@ -29,12 +29,13 @@ partial class In_Letters_Form
     private void InitializeComponent()
     {
         components = new System.ComponentModel.Container();
+        DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
         Top_Toolstrip = new ToolStrip();
         Bottom_ToolStrip = new ToolStrip();
         dataGridView1 = new DataGridView();
         iDDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-        subjectDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
         regDateDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+        subjectDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
         letterNumberDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
         letterTimeDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
         recivedDateDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
@@ -81,12 +82,20 @@ partial class In_Letters_Form
         // 
         // dataGridView1
         // 
+        dataGridView1.AllowUserToAddRows = false;
+        dataGridView1.AllowUserToDeleteRows = false;
+        dataGridView1.AllowUserToOrderColumns = true;
+        dataGridViewCellStyle1.BackColor = Color.WhiteSmoke;
+        dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
         dataGridView1.AutoGenerateColumns = false;
+        dataGridView1.BackgroundColor = Color.WhiteSmoke;
         dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-        dataGridView1.Columns.AddRange(new DataGridViewColumn[] { iDDataGridViewTextBoxColumn, subjectDataGridViewTextBoxColumn, regDateDataGridViewTextBoxColumn, letterNumberDataGridViewTextBoxColumn, letterTimeDataGridViewTextBoxColumn, recivedDateDataGridViewTextBoxColumn, letterTypeDataGridViewTextBoxColumn, senderDataGridViewTextBoxColumn, recipientDataGridViewTextBoxColumn, reciveMethodDataGridViewTextBoxColumn, groupNameDataGridViewTextBoxColumn, priorityDataGridViewTextBoxColumn, pageCountDataGridViewTextBoxColumn, deadlineDataGridViewTextBoxColumn, refDocDataGridViewTextBoxColumn, docSummaryDataGridViewTextBoxColumn, userNameDataGridViewTextBoxColumn, userIdDataGridViewTextBoxColumn, sysDateDataGridViewTextBoxColumn, sysTimeDataGridViewTextBoxColumn });
+        dataGridView1.Columns.AddRange(new DataGridViewColumn[] { iDDataGridViewTextBoxColumn, regDateDataGridViewTextBoxColumn, subjectDataGridViewTextBoxColumn, letterNumberDataGridViewTextBoxColumn, letterTimeDataGridViewTextBoxColumn, recivedDateDataGridViewTextBoxColumn, letterTypeDataGridViewTextBoxColumn, senderDataGridViewTextBoxColumn, recipientDataGridViewTextBoxColumn, reciveMethodDataGridViewTextBoxColumn, groupNameDataGridViewTextBoxColumn, priorityDataGridViewTextBoxColumn, pageCountDataGridViewTextBoxColumn, deadlineDataGridViewTextBoxColumn, refDocDataGridViewTextBoxColumn, docSummaryDataGridViewTextBoxColumn, userNameDataGridViewTextBoxColumn, userIdDataGridViewTextBoxColumn, sysDateDataGridViewTextBoxColumn, sysTimeDataGridViewTextBoxColumn });
         dataGridView1.DataSource = in_letters_bindingSource1;
         dataGridView1.Location = new Point(12, 73);
         dataGridView1.Name = "dataGridView1";
+        dataGridView1.ReadOnly = true;
+        dataGridView1.RowHeadersWidth = 40;
         dataGridView1.RowTemplate.Height = 25;
         dataGridView1.Size = new Size(1052, 461);
         dataGridView1.TabIndex = 2;
@@ -97,120 +106,142 @@ partial class In_Letters_Form
         iDDataGridViewTextBoxColumn.HeaderText = "ID";
         iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
         iDDataGridViewTextBoxColumn.ReadOnly = true;
-        // 
-        // subjectDataGridViewTextBoxColumn
-        // 
-        subjectDataGridViewTextBoxColumn.DataPropertyName = "Subject";
-        subjectDataGridViewTextBoxColumn.HeaderText = "Subject";
-        subjectDataGridViewTextBoxColumn.Name = "subjectDataGridViewTextBoxColumn";
+        iDDataGridViewTextBoxColumn.Width = 70;
         // 
         // regDateDataGridViewTextBoxColumn
         // 
         regDateDataGridViewTextBoxColumn.DataPropertyName = "RegDate";
         regDateDataGridViewTextBoxColumn.HeaderText = "RegDate";
         regDateDataGridViewTextBoxColumn.Name = "regDateDataGridViewTextBoxColumn";
+        regDateDataGridViewTextBoxColumn.ReadOnly = true;
+        // 
+        // subjectDataGridViewTextBoxColumn
+        // 
+        subjectDataGridViewTextBoxColumn.DataPropertyName = "Subject";
+        subjectDataGridViewTextBoxColumn.HeaderText = "Subject";
+        subjectDataGridViewTextBoxColumn.Name = "subjectDataGridViewTextBoxColumn";
+        subjectDataGridViewTextBoxColumn.ReadOnly = true;
+        subjectDataGridViewTextBoxColumn.Width = 125;
         // 
         // letterNumberDataGridViewTextBoxColumn
         // 
         letterNumberDataGridViewTextBoxColumn.DataPropertyName = "LetterNumber";
-        letterNumberDataGridViewTextBoxColumn.HeaderText = "LetterNumber";
+        letterNumberDataGridViewTextBoxColumn.HeaderText = "Letter Num";
         letterNumberDataGridViewTextBoxColumn.Name = "letterNumberDataGridViewTextBoxColumn";
+        letterNumberDataGridViewTextBoxColumn.ReadOnly = true;
+        letterNumberDataGridViewTextBoxColumn.Width = 125;
         // 
         // letterTimeDataGridViewTextBoxColumn
         // 
         letterTimeDataGridViewTextBoxColumn.DataPropertyName = "Letter_Time";
-        letterTimeDataGridViewTextBoxColumn.HeaderText = "Letter_Time";
+        letterTimeDataGridViewTextBoxColumn.HeaderText = "Letter Time";
         letterTimeDataGridViewTextBoxColumn.Name = "letterTimeDataGridViewTextBoxColumn";
+        letterTimeDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // recivedDateDataGridViewTextBoxColumn
         // 
         recivedDateDataGridViewTextBoxColumn.DataPropertyName = "RecivedDate";
         recivedDateDataGridViewTextBoxColumn.HeaderText = "RecivedDate";
         recivedDateDataGridViewTextBoxColumn.Name = "recivedDateDataGridViewTextBoxColumn";
+        recivedDateDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // letterTypeDataGridViewTextBoxColumn
         // 
         letterTypeDataGridViewTextBoxColumn.DataPropertyName = "LetterType";
         letterTypeDataGridViewTextBoxColumn.HeaderText = "LetterType";
         letterTypeDataGridViewTextBoxColumn.Name = "letterTypeDataGridViewTextBoxColumn";
+        letterTypeDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // senderDataGridViewTextBoxColumn
         // 
         senderDataGridViewTextBoxColumn.DataPropertyName = "Sender";
         senderDataGridViewTextBoxColumn.HeaderText = "Sender";
         senderDataGridViewTextBoxColumn.Name = "senderDataGridViewTextBoxColumn";
+        senderDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // recipientDataGridViewTextBoxColumn
         // 
         recipientDataGridViewTextBoxColumn.DataPropertyName = "Recipient";
         recipientDataGridViewTextBoxColumn.HeaderText = "Recipient";
         recipientDataGridViewTextBoxColumn.Name = "recipientDataGridViewTextBoxColumn";
+        recipientDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // reciveMethodDataGridViewTextBoxColumn
         // 
         reciveMethodDataGridViewTextBoxColumn.DataPropertyName = "Recive_Method";
         reciveMethodDataGridViewTextBoxColumn.HeaderText = "Recive_Method";
         reciveMethodDataGridViewTextBoxColumn.Name = "reciveMethodDataGridViewTextBoxColumn";
+        reciveMethodDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // groupNameDataGridViewTextBoxColumn
         // 
         groupNameDataGridViewTextBoxColumn.DataPropertyName = "GroupName";
         groupNameDataGridViewTextBoxColumn.HeaderText = "GroupName";
         groupNameDataGridViewTextBoxColumn.Name = "groupNameDataGridViewTextBoxColumn";
+        groupNameDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // priorityDataGridViewTextBoxColumn
         // 
         priorityDataGridViewTextBoxColumn.DataPropertyName = "Priority";
         priorityDataGridViewTextBoxColumn.HeaderText = "Priority";
         priorityDataGridViewTextBoxColumn.Name = "priorityDataGridViewTextBoxColumn";
+        priorityDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // pageCountDataGridViewTextBoxColumn
         // 
         pageCountDataGridViewTextBoxColumn.DataPropertyName = "PageCount";
         pageCountDataGridViewTextBoxColumn.HeaderText = "PageCount";
         pageCountDataGridViewTextBoxColumn.Name = "pageCountDataGridViewTextBoxColumn";
+        pageCountDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // deadlineDataGridViewTextBoxColumn
         // 
         deadlineDataGridViewTextBoxColumn.DataPropertyName = "Deadline";
         deadlineDataGridViewTextBoxColumn.HeaderText = "Deadline";
         deadlineDataGridViewTextBoxColumn.Name = "deadlineDataGridViewTextBoxColumn";
+        deadlineDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // refDocDataGridViewTextBoxColumn
         // 
         refDocDataGridViewTextBoxColumn.DataPropertyName = "Ref_Doc";
         refDocDataGridViewTextBoxColumn.HeaderText = "Ref_Doc";
         refDocDataGridViewTextBoxColumn.Name = "refDocDataGridViewTextBoxColumn";
+        refDocDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // docSummaryDataGridViewTextBoxColumn
         // 
         docSummaryDataGridViewTextBoxColumn.DataPropertyName = "DocSummary";
         docSummaryDataGridViewTextBoxColumn.HeaderText = "DocSummary";
         docSummaryDataGridViewTextBoxColumn.Name = "docSummaryDataGridViewTextBoxColumn";
+        docSummaryDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // userNameDataGridViewTextBoxColumn
         // 
         userNameDataGridViewTextBoxColumn.DataPropertyName = "UserName";
         userNameDataGridViewTextBoxColumn.HeaderText = "UserName";
         userNameDataGridViewTextBoxColumn.Name = "userNameDataGridViewTextBoxColumn";
+        userNameDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // userIdDataGridViewTextBoxColumn
         // 
         userIdDataGridViewTextBoxColumn.DataPropertyName = "UserId";
         userIdDataGridViewTextBoxColumn.HeaderText = "UserId";
         userIdDataGridViewTextBoxColumn.Name = "userIdDataGridViewTextBoxColumn";
+        userIdDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // sysDateDataGridViewTextBoxColumn
         // 
         sysDateDataGridViewTextBoxColumn.DataPropertyName = "SysDate";
         sysDateDataGridViewTextBoxColumn.HeaderText = "SysDate";
         sysDateDataGridViewTextBoxColumn.Name = "sysDateDataGridViewTextBoxColumn";
+        sysDateDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // sysTimeDataGridViewTextBoxColumn
         // 
         sysTimeDataGridViewTextBoxColumn.DataPropertyName = "SysTime";
         sysTimeDataGridViewTextBoxColumn.HeaderText = "SysTime";
         sysTimeDataGridViewTextBoxColumn.Name = "sysTimeDataGridViewTextBoxColumn";
+        sysTimeDataGridViewTextBoxColumn.ReadOnly = true;
         // 
         // in_letters_bindingSource1
         // 
@@ -257,9 +288,10 @@ partial class In_Letters_Form
     private DataGridView dataGridView1;
     private DataSet.Letters letters1;
     private BindingSource in_letters_bindingSource1;
+    private DataSet.LettersTableAdapters.Sa_In_LettersTableAdapter sa_In_LettersTableAdapter1;
     private DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
-    private DataGridViewTextBoxColumn subjectDataGridViewTextBoxColumn;
     private DataGridViewTextBoxColumn regDateDataGridViewTextBoxColumn;
+    private DataGridViewTextBoxColumn subjectDataGridViewTextBoxColumn;
     private DataGridViewTextBoxColumn letterNumberDataGridViewTextBoxColumn;
     private DataGridViewTextBoxColumn letterTimeDataGridViewTextBoxColumn;
     private DataGridViewTextBoxColumn recivedDateDataGridViewTextBoxColumn;
@@ -277,5 +309,4 @@ partial class In_Letters_Form
     private DataGridViewTextBoxColumn userIdDataGridViewTextBoxColumn;
     private DataGridViewTextBoxColumn sysDateDataGridViewTextBoxColumn;
     private DataGridViewTextBoxColumn sysTimeDataGridViewTextBoxColumn;
-    private DataSet.LettersTableAdapters.Sa_In_LettersTableAdapter sa_In_LettersTableAdapter1;
 }
