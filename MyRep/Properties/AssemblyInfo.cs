fe7aec823e85c07d10b296151f5a0e4c75671f2d
Iggy988 <igor.rodic1988@gmail.com>
2024-01-20 21:39:58 +0100

    assigning versioning information to project

--- MyRep/Properties/AssemblyInfo.cs
+++ MyRep/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyRep")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("My Reports")]
+[assembly: AssemblyDescription("---")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyRep")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("CoC Group")]
+[assembly: AssemblyProduct("My Reports Generator")]
+[assembly: AssemblyCopyright("Copyright ©  2020-2024")]
+[assembly: AssemblyTrademark("CoC")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2024.01.20.2")]
+[assembly: AssemblyFileVersion("2024.01.20.1")]
